f706898092de19779135458aa7aa7376024db710
Đăng Huy <lphan7024@gmail.com>
2025-11-26 22:53:20 +0700

    Add files via upload

---
+++ BTTH_Buoi1/Bai1/data.php
@@ -0,0 +1,47 @@
+<?php
+$flowers = [
+    [
+        "name" => "Hoa Đỗ Quyên",
+        "desc" => "Hoa đỗ quyên nở rực rỡ với nhiều màu sắc, tượng trưng cho sự may mắn và hạnh phúc.",
+        "image" => "doquyen.jpg"
+    ],
+    [
+        "name" => "Hoa Hải Đường",
+        "desc" => "Hoa hải đường mang vẻ đẹp sang trọng, thường nở vào dịp Tết, tượng trưng cho phú quý và thịnh vượng.",
+        "image" => "haiduong.jpg"
+    ],
+    [
+        "name" => "Hoa Mai",
+        "desc" => "Hoa mai vàng rực rỡ, biểu tượng của mùa xuân và sự may mắn trong năm mới.",
+        "image" => "mai.jpg"
+    ],
+    [
+        "name" => "Hoa Tường Vy",
+        "desc" => "Hoa tường vy có hương thơm dịu nhẹ, cánh mỏng manh tượng trưng cho sự tinh tế và dịu dàng.",
+        "image" => "tuongvy.jpg"
+    ],
+    [
+        "name" => "Hoa dạ yến thảo",
+        "desc" => "Dạ yến thảo là lựa chọn thích hợp cho những ai yêu thích trồng hoa làm đẹp nhà ở. Hoa có thể nở rực quanh năm, kể cả tiết trời se lạnh của mùa xuân hay cả những ngày nắng nóng cao điểm của mùa hè. Dạ yến thảo được trồng ở chậu treo nơi cửa sổ hay ban công, dáng hoa mềm mại, sắc màu đa dạng nên được yêu thích vô cùng.",
+        "image" => "dayeuthao.png"
+    ],
+    [
+        "name" => "Hoa đồng tiền",
+        "desc" => "Hoa đồng tiền thích hợp để trồng trong mùa xuân và đầu mùa hè, khi mà cường độ ánh sáng chưa quá mạnh. Cây có hoa to, nở rộ rực rỡ, lại khá dễ trồng và chăm sóc nên sẽ là lựa chọn thích hợp của bạn trong tiết trời này. Về mặt ý nghĩa, hoa đồng tiền cũng tượng trưng cho sự sung túc, tình yêu và hạnh phúc viên mãn.",
+        "image" => "dongtien.png"
+    ],
+    [
+        "name" => "Hoa giấy",
+        "desc" => "Hoa giấy có mặt ở hầu khắp mọi nơi trên đất nước ta, thích hợp với nhiều điều kiện sống khác nhau nên rất dễ trồng, không tốn quá nhiều công chăm sóc nhưng thành quả mang lại sẽ khiến bạn vô cùng hài lòng. Hoa giấy mỏng manh nhưng rất lâu tàn, với nhiều màu như trắng, xanh, đỏ, hồng, tím, vàng… cùng nhiều sắc độ khác nhau. Vào mùa khô hanh, hoa vẫn tươi sáng trên cây khiến ngôi nhà luôn luôn bừng sáng.
+ ",
+        "image" => "giay.png"
+    ],
+    [
+        "name" => "Hoa thanh tú",
+        "desc" => "Mang dáng hình tao nhã, màu sắc thiên thanh dịu dàng của hoa thanh tú có thể khiến bạn cảm thấy vô cùng nhẹ nhàng khi nhìn ngắm. Cây khá dễ trồng, lại nở nhiều hoa cùng một lúc, từ một bụi nhỏ có thể đâm nhánh, tạo nên những cây con phát triển sum suê. Thanh tú trồng ở nơi có nắng sẽ ra hoa nhiều, vì thế thích hợp trong cả mùa xuân lẫn mùa hè, đem lại khoảng không gian xanh mát cho ngôi nhà ngày oi nóng.",
+        "image" => "thanhtu.png"
+    ],
+
+    
+];
+?>
